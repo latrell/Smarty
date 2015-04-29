9433d349e5bcdd1b83afe52305915837806cdd48
Dave Thomas <dave@monstergfx.com>
2015-04-29 11:18:10 -0700

    set escape_html flag from configuration value

--- src/Latrell/Smarty/SmartyEngine.php
+++ src/Latrell/Smarty/SmartyEngine.php
@@ -68,7 +68,10 @@ class SmartyEngine implements Engines\EngineInterface
 		$smarty->cache_lifetime = $cache_lifetime;
 		$smarty->compile_check = true;
 
-		// $smarty->escape_html = true;
+		// set the escape_html flag from the configuration value
+		//
+		$smarty->escape_html = $escape_html;
+
 		$smarty->error_reporting = E_ALL & ~ E_NOTICE;
 
 		foreach ($__data as $var => $val) {
